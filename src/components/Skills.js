f597109700b40bde2096ee82e55007fdf9ca6b55
Berk Bozok <berk_bozok@live.com>
2022-08-11 22:24:15 -0400

    adjustment

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -10,6 +10,9 @@ const Skills = () => {
         <div
           className='grid grid-cols-8 md:grid-flow-col'
         >
+
+
+          
           {skills.map((skill, index) => {
             return (
             
