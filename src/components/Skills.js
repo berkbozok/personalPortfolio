59ff77e7d1f7e7bfcff70775ccba1cc94049d26f
Berk Bozok <berk_bozok@live.com>
2022-09-01 23:59:05 -0400

    skills added

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -12,14 +12,6 @@ const Skills = () => {
           {skills.map((skill, index) => {
             return (
 
-
-
-
-
-
-
-
-              
               
               <div
                 className='flex items-center justify-center'
