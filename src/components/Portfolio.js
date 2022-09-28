ed4956b655266a45293ec296dd74c5f42331cda1
Berk Bozok <berk_bozok@live.com>
2022-09-28 11:25:02 -0400

    2nd skills added

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -15,6 +15,10 @@ const Portfolio = () => {
 
 
 
+
+
+
+
           <p className='subtitle'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga veniam
             labore nisium illum cupiditate reiciendis a numquam
