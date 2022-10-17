4c196476d222a976bcc34207be49f2716193b25a
Berk Bozok <berk_bozok@live.com>
2022-10-17 00:12:32 -0400

    2nd skills added

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -10,6 +10,10 @@ const Portfolio = () => {
         <div className='flex flex-col items-center text-center'>
           <h2 className='section-title before:content-portfolio relative before:absolute before:opacity-40 before:-top-[2rem] before:-left-3/4 before:hidden before:lg:block'>
             My latest work
+
+
+
+            
           </h2>
           <p className='subtitle'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga veniam
