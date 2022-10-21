13eb5e7da2b13c15d467f2b60b767f2b52d93b78
Berk Bozok <berk_bozok@live.com>
2022-10-20 23:12:02 -0400

    adjustment

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -13,6 +13,9 @@ const Portfolio = () => {
 
 
           </h2>
+
+
+          
           <p className='subtitle'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga veniam
             labore nisium illum cupiditate reiciendis a numquam
