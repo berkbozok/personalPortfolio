1409fb63f4c1f9c0165859bd8cd294d6035a5e71
Berk Bozok <berk_bozok@live.com>
2022-11-14 01:15:25 -0500

    Syntax error fixed

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -7,6 +7,9 @@ const Socials = () => {
     <ul className='flex space-x-6'>{social.map((item,index)=>{
             return <li className='flex justify-center items-center text-accent' 
             key={index}>
+
+
+              
                 <a className='text-base' href={item.href}>
                 {item.icon}
                 </a>
