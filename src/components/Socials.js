0b406ad89772e8e1da40983aa72d3125a57d2d09
Berk Bozok <berk_bozok@live.com>
2023-03-02 21:41:35 -0500

    code clear v2

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -7,7 +7,6 @@ const Socials = () => {
     <ul className='flex space-x-6'>{social.map((item,index)=>{
             return <li className='flex justify-center items-center text-accent' 
             key={index}>
-
                 <a className='text-base' href={item.href}>
                 {item.icon}
                 </a>
