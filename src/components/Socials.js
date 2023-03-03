e2c5ee47aaeb2f1dad8093519c42d9155ad6c567
Berk Bozok <berk_bozok@live.com>
2023-03-02 21:43:06 -0500

    document formatted

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,21 +1,24 @@
-import React from 'react'
+import React from "react";
 
-import {social} from '../data'
+import { social } from "../data";
 
 const Socials = () => {
   return (
-    <ul className='flex space-x-6'>{social.map((item,index)=>{
-            return <li className='flex justify-center items-center text-accent' 
-            key={index}>
-                <a className='text-base' href={item.href}>
-                {item.icon}
-                </a>
-            </li>
+    <ul className="flex space-x-6">
+      {social.map((item, index) => {
+        return (
+          <li
+            className="flex justify-center items-center text-accent"
+            key={index}
+          >
+            <a className="text-base" href={item.href}>
+              {item.icon}
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
-    }
-        
-        )}</ul>
-  )
-}
-
-export default Socials
+export default Socials;
