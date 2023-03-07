f329fc7e5ff93b9a8adeaa892de3f991d38e86f3
Berk Bozok <berk_bozok@live.com>
2023-03-07 11:08:39 -0500

    brand adjusted

--- src/components/Brands.js
+++ src/components/Brands.js
@@ -1,20 +1,14 @@
-import React from 'react';
-
-// import brands data
-import { brands } from '../data';
+import React from "react";
+import { brands } from "../data";
 
 const Brands = () => {
   return (
-    <section className='min-h-[146px] bg-tertiary flex items-center'>
-      <div
-        className='container mx-auto flex md:justify-between items-center flex-wrap justify-evenly'
-      >
+    <section className="min-h-[146px] bg-tertiary flex items-center">
+      <div className="container mx-auto flex md:justify-between items-center flex-wrap justify-evenly">
         {brands.map((brand, index) => {
           return (
-            <div
-              key={index}
-            >
-              <img src={brand.img} alt='' />
+            <div key={index}>
+              <img src={brand.img} alt="" />
             </div>
           );
         })}
