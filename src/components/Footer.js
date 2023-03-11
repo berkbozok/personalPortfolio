e9c4529f1a9962ea25b8d9739f3ab60b0a2b0c78
Berk Bozok <berk_bozok@live.com>
2023-03-11 01:48:32 -0500

    footer added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,6 +8,7 @@ import Logo from "../assets/img/logo.svg";
 
 const Footer = () => {
   return (
+    
     <footer className="bg-tertiary py-12">
       <div className="container mx-auto">
         <div className="flex flex-col lg:flex-row space-y-6 lg:space-y-0 items-center justify-between">
