5e9ae859d05a4f5f1c1e5328c061dd281b1ea3cd
Berk Bozok <berk_bozok@live.com>
2023-03-12 00:33:15 -0500

    import updated

--- src/components/NavMobile.js
+++ src/components/NavMobile.js
@@ -1,19 +1,9 @@
 import React, { useState } from "react";
-
-// import icons
 import { XIcon } from "@heroicons/react/outline";
 import { MenuAlt3Icon } from "@heroicons/react/outline";
-
-// import navigation data
 import { navigation } from "../data";
-
-// import components
 import Socials from "./Socials";
-
-// import framer
 import { motion } from "framer-motion";
-
-// import Link
 import { Link } from "react-scroll";
 
 const NavMobile = () => {
