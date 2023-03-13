78b032aa8679631a3a2317efeec4f697e6875040
Berk Bozok <berk_bozok@live.com>
2023-03-13 02:01:36 -0400

    refactor

--- src/App.js
+++ src/App.js
@@ -1,37 +1,31 @@
-import React from 'react';
-import About from './components/About';
-import  Brands  from './components/Brands';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import Portfolio from './components/Portfolio';
-import Skills from './components/Skills';
-import Services from './components/Services';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import BackTopBtn from './components/BackToTopBtn';
-
+import React from "react";
+import About from "./components/About";
+import Brands from "./components/Brands";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Portfolio from "./components/Portfolio";
+import Skills from "./components/Skills";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import BackTopBtn from "./components/BackToTopBtn";
 
 const App = () => {
-
- 
-  return <div>
-    
-    <Header/>
-    <Hero/>
-    <Brands/>
-    <About/>
-    <Skills/>
-    <Portfolio/>
-    <Services/>
-    <Contact/>
-    <Footer/>
-    <BackTopBtn/>
-    <div style={{height:'000px'}}>
-
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <Brands />
+      <About />
+      <Skills />
+      <Portfolio />
+      <Services />
+      <Contact />
+      <Footer />
+      <BackTopBtn />
+      <div style={{ height: "000px" }}></div>
     </div>
-
-
-  </div>;
+  );
 };
 
 export default App;
