93eb0b1add082e57522b683ecfafdea2ef58769a
berkbozok <berk_bozok@live.com>
2024-03-11 13:23:35 -0400

    test

--- src/data.js
+++ src/data.js
@@ -293,6 +293,7 @@ export const contact = [
     subtitle: "I am here to help you.",
     description: "Email me at berk_bozok@live.com",
   },
+
   {
     icon: <FiMapPin />,
     title: "Current Location",
