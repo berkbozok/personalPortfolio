fa244b0b7f6fc5ec7372da409b25ac07a7d801ea
berkbozok <berk_bozok@live.com>
2024-04-07 10:32:13 -0400

    console log removed

--- src/components/BackToTopBtn.js
+++ src/components/BackToTopBtn.js
@@ -18,7 +18,7 @@ const BackTopBtn = () => {
     return (
       // if needed set it to show
       //
-      //change false to show
+      //change false to showw
       false && ( //show
         <button
           onClick={() => scrollToTop()}
