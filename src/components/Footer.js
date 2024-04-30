ca0f84b55ca21baa197874ddeafd5e4b5ce95f66
berkbozok <berk_bozok@live.com>
2024-04-30 15:00:03 -0400

    slider improved

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,8 @@
 import React from "react";
 import { social } from "../data";
-import Logo from "../assets/img/logo.svg";
 
 const Footer = () => {
   return (
-
     <footer className="bg-tertiary py-12">
       <div className="container mx-auto">
         <div className="flex flex-col lg:flex-row space-y-6 lg:space-y-0 items-center justify-between">
