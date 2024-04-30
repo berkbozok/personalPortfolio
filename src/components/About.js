0d115e45b4cf026f63ba0f290dd9505a888613d0
berkbozok <berk_bozok@live.com>
2024-04-30 14:59:35 -0400

    import

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,7 @@ import { Link } from "react-scroll";
 
 const About = () => {
   // testing
+  // information
   return (
     <section className="section bg-secondary" id="about">
       <div className="container mx-auto">
