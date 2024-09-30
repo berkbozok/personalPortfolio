1bc82ac9471d4de6e692938282ce4331ba2b93b7
berkbozok <berk@musicredone.com>
2024-09-29 23:46:12 -0400

    update

--- src/components/BackToTopBtn.js
+++ src/components/BackToTopBtn.js
@@ -1,3 +1,4 @@
+// Imports
 import React, { useState, useEffect } from "react";
 import { Link, animateScroll as scroll } from "react-scroll";
 import { ChevronUpIcon } from "@heroicons/react/outline";
