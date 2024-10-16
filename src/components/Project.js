41ee34f7347d1b24f1d70c9a27ec5d36a44c4b52
berkbozok <berk_bozok@live.com>
2024-10-15 21:05:08 -0400

    code cleaned

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,14 +1,19 @@
-import React from 'react';
+// Imports
+import React from "react";
 
 const Project = ({ item }) => {
   return (
-    <div key={item.id} className='flex flex-col items-center text-center'>
-      <div className='mb-8'>
-        <img className='rounded-2xl object-cover h-48 w-96 ' src={item.image} alt='' />
+    <div key={item.id} className="flex flex-col items-center text-center">
+      <div className="mb-8">
+        <img
+          className="rounded-2xl object-cover h-48 w-96 "
+          src={item.image}
+          alt=""
+        />
       </div>
-      <p className='capitalize text-accent text-sm mb-3'>{item.category}</p>
-      <h3 className='text-2xl font-semibold capitalize mb-3'>{item.name}</h3>
-      <p className='text-base max-w-md'>{item.detail}</p>
+      <p className="capitalize text-accent text-sm mb-3">{item.category}</p>
+      <h3 className="text-2xl font-semibold capitalize mb-3">{item.name}</h3>
+      <p className="text-base max-w-md">{item.detail}</p>
     </div>
   );
 };
